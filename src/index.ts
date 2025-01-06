9f8c75839c0b726340abedc3972a4166fbf4da6d
unknown <rohansharmarohansharma22@gmail.com>
2025-01-06 14:20:36 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from "express"
+import jwt from "jsonwebtoken"
+
+const app=express()
+
+app.post("api/v1/signup",(res,req) =>{
+
+})
+
+app.post("api/v1/login",(res,req) =>{
+
+})
+
+app.post("api/v1/content",(res,req) =>{
+
+})
+
+app.get("api/v1/content",(res,req) =>{
+
+})
+
+app.delete("api/v1/content",(res,req) =>{
+
+})
+
+app.post("api/v1/brain/share",(res,req) =>{
+
+})
+
+app.get("api/v1/brain/:shareLink",(res,req) =>{
+    
+})
